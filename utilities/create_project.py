d3df997f1b5da2f74031e708eb559a6ff6a8c420
omvss <omvaishno.0@gmail.com>
2025-09-24 14:01:05 +0530

    added create_project

---
+++ utilities/create_project.py
@@ -0,0 +1,24 @@
+# utilities/create_project.py
+import os
+import sys
+import subprocess
+
+def create_django_project(project_name):
+    try:
+        if not os.path.exists(project_name):
+            os.makedirs(project_name)
+            
+        subprocess.run(
+            ["django-admin", "startproject", "config", project_name],
+            check=True
+        )
+        print(f" Django project '{project_name}' created successfully!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error while creating project: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python utilities/create_project.py <project_name>")
+    else:
+        project_name = sys.argv[1]
+        create_django_project(project_name)
